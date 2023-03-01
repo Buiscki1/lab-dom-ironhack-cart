a0b814b75ed9a0a53952c7b6b17bb08753f71417
Buiscki1 <122836250+Buiscki1@users.noreply.github.com>
2023-03-02 00:41:22 +0100

    Finished, bonus not included.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
 // ITERATION 1
 
-function updateSubtotal(product) {
-  const price = product.querySelector('.price span');
-  const quantity = document.querySelector('input').value;
-  const subtotal = product.querySelector(".subtotal span");
-  let actualPrice = price.innerText;
-  let updatedSubtotal = actualPrice*quantity; 
-  subtotal.innerText = updatedSubtotal;
-  return  updatedSubtotal;
+function updateSubtotalFunction(product) {
+  console.log("Calculating subtotal, yey!");
+  //Use product.querry for everything, so that it selects the children of the product thats being passed in
+  let price = product.querySelector(".price span");
+  let quantity = parseFloat(product.querySelector(".quantity input").value);
+  let subtotal = product.querySelector(".subtotal span");
+  //use parse float to convert to a number
+  let actualPrice = parseFloat(price.innerText);
+  let subtotalValue  = actualPrice * quantity;
+  subtotal.innerText = `${Math.round(subtotalValue  * 100) / 100}`;
+  return subtotalValue;
 }
 
 function calculateAll() {
-  // ITERATION 2
-  const singleProduct = document.getElementsByClassName('.product')[0];
-  updateSubtotal(singleProduct);
-  // ITERATION 3
-  //... your code goes here
+  // Select all the product rows in the table
+  const products = document.querySelectorAll(".product");
+
+  // Loop through each product row and update its subtotal
+  let total = 0;
+
+  products.forEach((product) => {
+    const subtotal = updateSubtotalFunction(product);
+    total += subtotal;
+  })
+  document.querySelector("#total-value span").innerText = Math.round(total * 100) / 100;
 }
 
 // ITERATION 4
@@ -32,10 +41,6 @@ function createProduct() {
   //... your code goes here
 }
 
-window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
-
-  //... your code goes here
-});
+const calculatePricesBtn = document.getElementById("calculate");
+calculatePricesBtn.addEventListener("click", calculateAll);
 
